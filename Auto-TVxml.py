c69ac5f5d3cada53aeb9fa10c49f85f3ef44cb37
chenzj511 <52043308+chenzj511@users.noreply.github.com>
2024-07-17 18:47:57 +0800

    Update Auto-TVxml.py

--- Auto-TVxml.py
+++ Auto-TVxml.py
@@ -55,10 +55,9 @@ def extract_tvg_info_from_m3u(m3u_data):
 def epg_api_data(tvg_id,tvg_name):
     epg_date=requests.get(epg1_api+tvg_name,headers=header)
     if tvg_name == 'CCTV1':
-        s =  epg_date.json()['epg_data'][0].["title"]
-        if isinstance(s, unicode): 
-           print("unicode")
-           print s.encode('gb2312')  
+        s = epg_date.json()['epg_data'][0].["title"]
+        if isinstance(s,unicode):
+            print (s.encode('gb2312'),'unicode')
         else:
            s.decode('utf-8').encode('gb2312')  
   #    print(tvg_name, '==',codecs.encode(str(epg_date.json()['epg_data'][0]),'gb2312') , '!!\n')
