7a58f37b38ce1c84009814a6097a9533b29caa18
chenzj511 <52043308+chenzj511@users.noreply.github.com>
2024-07-17 14:41:00 +0800

    Update Auto-TVxml.py

--- Auto-TVxml.py
+++ Auto-TVxml.py
@@ -54,10 +54,10 @@ def extract_tvg_info_from_m3u(m3u_data):
 def epg_api_data(tvg_id,tvg_name):
     epg_date=requests.get(epg1_api+tvg_name,headers=header)
    # if tvg_id == 'F050':
-    print(tvg_name, '==',epg_date.content.decode('gbk') , '!!\n')
+    print(tvg_name, '==',epg_date.content.decode('gb2312') , '!!\n')
     search_string = "精彩节目"
     
-    str_title = epg_date.content.decode('gbk')
+    str_title = epg_date.content.decode('GBK')
     if '精彩节目' in str_title or tvg_name in '卡酷少儿 纪实科教':
         print(tvg_name,'的EPG节目信息在API1中不存在或不准确 已更换为API2')
         epg_date=requests.get(epg2_api+tvg_name,headers=header)
